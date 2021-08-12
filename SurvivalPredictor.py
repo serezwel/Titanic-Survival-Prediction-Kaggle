453aab34a2d13fd52833ca939fde6fcdc8b23951
Andrew Ngadiman <serezwel@gmail.com>
2021-08-12 22:14:33 +0700

    Added age imputation using group by Pclass

--- SurvivalPredictor.py
+++ SurvivalPredictor.py
@@ -3,13 +3,39 @@ from sklearn.ensemble import RandomForestClassifier
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+def impute_age(cols):
+    age = cols[0]
+    Pclass = cols[1]
+    if pd.isna(age):
+        if Pclass == 1:
+            return 38
+        elif Pclass == 2:
+            return 30
+        else:
+            return 25
+    else:
+        return age
+        
+
 train_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
+
 sns.heatmap(train_data.isna(), cmap='viridis', yticklabels=False, cbar=False)
 plt.savefig("CheckEmpty.png")
+plt.clf()
+sns.boxplot(x = "Pclass", y = "Age", data=train_data)
+plt.savefig("AgeCheck.png")
+plt.clf()
 y = train_data["Survived"]
 
-features = ["Pclass", "Sex", "SibSp", "Parch"]
+imputing = train_data[["Age", "Pclass"]]
+mean = imputing.groupby(["Pclass"]).mean().round()
+print(mean)
+train_data["Age"] = imputing.apply(impute_age, axis = 1)
+test_data["Age"] = test_data[["Age", "Pclass"]].apply(impute_age, axis = 1)
+
+features = ["Pclass", "Sex", "SibSp", "Parch", "Age"]
 X = pd.get_dummies(train_data[features])
 X_test = pd.get_dummies(test_data[features])
 
