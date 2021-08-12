e0149c0011e22d6997e59954b0f0949ebe2a48f2
Andrew Ngadiman <serezwel@gmail.com>
2021-08-12 21:43:43 +0700

    Tried sample solution from tutorial and created plot for checking empty data

--- SurvivalPredictor.py
+++ SurvivalPredictor.py
@@ -1,6 +1,21 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 train_data = pd.read_csv("train.csv")
+test_data = pd.read_csv("test.csv")
 sns.heatmap(train_data.isna(), cmap='viridis', yticklabels=False, cbar=False)
+plt.savefig("CheckEmpty.png")
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators = 100, max_depth = 5, random_state = 1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({"PassengerId": test_data.PassengerId, "Survived": predictions})
+output.to_csv("my_submission.csv", index=False)
