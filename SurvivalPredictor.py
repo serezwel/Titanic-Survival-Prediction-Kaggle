3fb55baef92954db1dc2c7bf88b7a66914340fa0
Andrew Ngadiman <serezwel@gmail.com>
2021-08-13 23:03:36 +0700

    Update SurvivalPredictor.py

--- SurvivalPredictor.py
+++ SurvivalPredictor.py
@@ -17,31 +17,34 @@ def impute_age(cols):
     else:
         return age
         
+def main():
+    train_data = pd.read_csv("train.csv")
+    test_data = pd.read_csv("test.csv")
 
-train_data = pd.read_csv("train.csv")
-test_data = pd.read_csv("test.csv")
+    sns.heatmap(train_data.isna(), cmap='viridis', yticklabels=False, cbar=False)
+    plt.savefig("CheckEmpty.png")
+    plt.clf()
+    sns.boxplot(x = "Pclass", y = "Age", data=train_data)
+    plt.savefig("AgeCheck.png")
+    plt.clf()
+    y = train_data["Survived"]
 
-sns.heatmap(train_data.isna(), cmap='viridis', yticklabels=False, cbar=False)
-plt.savefig("CheckEmpty.png")
-plt.clf()
-sns.boxplot(x = "Pclass", y = "Age", data=train_data)
-plt.savefig("AgeCheck.png")
-plt.clf()
-y = train_data["Survived"]
+    imputing = train_data[["Age", "Pclass"]]
+    mean = imputing.groupby(["Pclass"]).mean().round()
+    print(mean)
+    train_data["Age"] = imputing.apply(impute_age, axis = 1)
+    test_data["Age"] = test_data[["Age", "Pclass"]].apply(impute_age, axis = 1)
 
-imputing = train_data[["Age", "Pclass"]]
-mean = imputing.groupby(["Pclass"]).mean().round()
-print(mean)
-train_data["Age"] = imputing.apply(impute_age, axis = 1)
-test_data["Age"] = test_data[["Age", "Pclass"]].apply(impute_age, axis = 1)
+    features = ["Pclass", "Sex", "SibSp", "Parch", "Age"]
+    X = pd.get_dummies(train_data[features])
+    X_test = pd.get_dummies(test_data[features])
 
-features = ["Pclass", "Sex", "SibSp", "Parch", "Age"]
-X = pd.get_dummies(train_data[features])
-X_test = pd.get_dummies(test_data[features])
+    model = RandomForestClassifier(n_estimators = 100, max_depth = 5, random_state = 1)
+    model.fit(X, y)
+    predictions = model.predict(X_test)
 
-model = RandomForestClassifier(n_estimators = 100, max_depth = 5, random_state = 1)
-model.fit(X, y)
-predictions = model.predict(X_test)
+    output = pd.DataFrame({"PassengerId": test_data.PassengerId, "Survived": predictions})
+    output.to_csv("my_submission.csv", index=False)
 
-output = pd.DataFrame({"PassengerId": test_data.PassengerId, "Survived": predictions})
-output.to_csv("my_submission.csv", index=False)
+if __name__ == "__main__":
+    main()
